6f05afd744f3d3d6f2fb05ee840bbadc3b5d8021
Keith Bishop <Keith.Bishop33@gmail.com>
2025-04-01 13:44:48 -0500

    add screen

--- main.py
+++ main.py
@@ -1,10 +1,26 @@
 import pygame
-from constants import screen_size
+from constants import *
 
 def main():
     print("Starting Asteroids!")
-    screen_size()
-  
+    print(f"Screen width:{SCREEN_WIDTH}")
+    print(f"Screen height:{SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    dt = 0.0
+
+    running = True
+    while running: # setting an infinite loop to keep the game running
+        for event in pygame.event.get(): # checking for events and window closure
+            if event.type == pygame.QUIT: 
+                running = False
+           
+        screen.fill("black")  # Fill the screen with black
+        pygame.display.flip()  # Update the display
+        dt = clock.tick(60) / 1000.0 # 60 FPS
+            
 
 if __name__ == "__main__":
     main()
